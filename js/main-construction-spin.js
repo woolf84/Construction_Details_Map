f7b601783e2d7c49945f877660d139831e80449c
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-11-06 23:06:10 -0500

    Update main-construction-spin.js

--- js/main-construction-spin.js
+++ js/main-construction-spin.js
@@ -7,15 +7,14 @@ var map = L.map('map',{
 });
 
 var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     minZoom: 0,
     maxZoom: 19,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     ext: 'png'
 }).addTo(map);
 
 var zoomControl = L.control.zoom({
-    position: 'bottomright'
+    position: 'topright'
 });
 map.addControl(zoomControl);
 
