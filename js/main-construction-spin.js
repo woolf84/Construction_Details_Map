8159bc39fe381daa8dfd673d159c725b813354a7
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-11-06 22:18:35 -0500

    change max zoom; add attribution

--- js/main-construction-spin.js
+++ js/main-construction-spin.js
@@ -2,13 +2,15 @@ var map = L.map('map',{
     center: [42.350,-71.065],
     zoom: 14,
     minZoom:4,
-    maxZoom: 21,
+    maxZoom: 19,
     zoomControl: false,
 });
 
 var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 0,
-    maxZoom: 21,
+    maxZoom: 19,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     ext: 'png'
 }).addTo(map);
 
